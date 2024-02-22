6ae0783ab5059978a4ca08f7ce8350e6ed38c2f8
Edoardo Fava <f.edoardo.0@gmail.com>
2024-02-22 18:49:32 +0100

    flight endpoint correct path and dynamic icao

--- app/api.js
+++ app/api.js
@@ -13,47 +13,62 @@ function acceptsSSE(ctx) {
     return ctx.get("Accept").toLowerCase() == "text/event-stream"
 }
 
-router.get("/hello", async ctx => {
-    ctx.set("Access-Control-Allow-Origin", "*")
-    if (acceptsSSE(ctx)) {
-        ctx.request.socket.setTimeout(0);
-        ctx.req.socket.setNoDelay(true);
-        ctx.req.socket.setKeepAlive(true);
-        ctx.set({
-            "Content-Type": "text/event-stream",
-            "Cache-Control": "no-cache",
-            "Connection": "keep-alive",
-        })
-        const sseStream = new stream.PassThrough()
-        ctx.status = 200
-        ctx.body = sseStream
-        try {
-            const disconnect = await consumeFlights("ELLX", "test"+Math.random(), (obj) => {
-                if (sseStream.closed || sseStream.destroyed) {
-                    obj.disconnect()
-                    return
-                }
-                try {
-                    sseStream.write(`data: ${JSON.stringify(obj)}\n\n`)
-                } catch (error) {
-                    console.error("stream write error", error)
-                    obj.disconnect()
-                }
+const airportIcao = new Set(["ELLX", "LIRF", "KATL"])
+
+function icaoOk(icao) {
+    return airportIcao.has(icao)
+}
+
+router.get("/airport/:icao/flights", async ctx => {
+    const icao = ctx.params.icao.toUpperCase()
+    if (icaoOk(icao)) {
+        ctx.set("Access-Control-Allow-Origin", "*")
+        if (acceptsSSE(ctx)) {
+            ctx.request.socket.setTimeout(0);
+            ctx.req.socket.setNoDelay(true);
+            ctx.req.socket.setKeepAlive(true);
+            ctx.set({
+                "Content-Type": "text/event-stream",
+                "Cache-Control": "no-cache",
+                "Connection": "keep-alive",
             })
-            sseStream.on("close", () => disconnect())
-        } catch (error) {
-            console.error("consumeFlights error", error)
+            const sseStream = new stream.PassThrough()
+            ctx.status = 200
+            ctx.body = sseStream
+            try {
+                const disconnect = await consumeFlights(icao, "test"+Math.random(), (obj) => {
+                    if (sseStream.closed || sseStream.destroyed) {
+                        obj.disconnect()
+                        return
+                    }
+                    try {
+                        sseStream.write(`data: ${JSON.stringify(obj)}\n\n`)
+                    } catch (error) {
+                        console.error("stream write error", error)
+                        obj.disconnect()
+                    }
+                })
+                sseStream.on("close", () => disconnect())
+            } catch (error) {
+                console.error("consumeFlights error", error)
+                ctx.status = 500
+                ctx.body = error.toString()
+                ctx.res.end() // manually end the connection since it's a stream
+            }
+        } else {
+            try {
+                const flights = await getLatestFlights(icao, "test"+Math.random())
+                ctx.set("Content-Type", "application/json;charset=utf-8")
+                ctx.body = JSON.stringify(flights)
+            } catch (error) {
+                console.error("getLatestFlights error", error)
+                ctx.status = 500
+                ctx.body = error.toString()
+            }
         }
     } else {
-        try {
-            const flights = await getLatestFlights("ELLX", "test"+Math.random())
-            ctx.set("Content-Type", "application/json;charset=utf-8")
-            ctx.body = JSON.stringify(flights)
-        } catch (error) {
-            console.error(error)
-            ctx.status = 500
-            ctx.body = error.toString()
-        }
+        ctx.status = 404
+        ctx.body = "icao not found"
     }
 })
 
